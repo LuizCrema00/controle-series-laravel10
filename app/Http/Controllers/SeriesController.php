66ccd7a97e337e61663e43bf58bc7f195b5bac1c
Luiz Guilherme Crema <lgcrema@hotmail.com>
2023-11-09 17:51:51 -0300

    feat(bootstrap): instalacao bootstrap

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     //
     public function index() {
-        
+        $series = Serie::all();
+        return view("series.index")->with('series', $series);
     }
 }
---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout title="Séries">
+
+    <ul>
+        @foreach ($series as $serie)
+        <li>{{ $serie }}</li>
+        @endforeach
+    </ul>
+    
+</x-layout>
+
